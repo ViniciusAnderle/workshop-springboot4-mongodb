586367e12b308d542d9091a60b745aed0687760f
Vinicius Anderle <vinianderle00@gmail.com>
2026-07-17 20:24:41 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/nelio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelio/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.nelio.workshopmongo.domain.Post;
 import com.nelio.workshopmongo.domain.User;
 import com.nelio.workshopmongo.dto.UserDTO;
 import com.nelio.workshopmongo.services.UserService;
@@ -68,11 +69,20 @@ public class UserResource {
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
 
-	    User obj = service.fromDto(objDto);
-	    obj.setId(id);
-	    obj = service.update(obj);
+		User obj = service.fromDto(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
 
-	    return ResponseEntity.noContent().build();
+		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+
+		User obj = service.findById(id);
+
+		return ResponseEntity.ok().body(obj.getPosts());
+
 	}
 
 }
